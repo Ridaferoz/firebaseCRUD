9db9074cb98d53ebddba80e1c35057059237b02c
Rida <ridaferoz2@gmail.com>
2018-09-03 12:35:48 -0700

    rida work

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CREDENTIALS = {
+    
+        apiKey: "AIzaSyA2DOPaCnYX6MfSXEEkRwC5u-itSCR37yc",
+        authDomain: "fir-crud-e8c9b.firebaseapp.com",
+        databaseURL: "https://fir-crud-e8c9b.firebaseio.com",
+        projectId: "fir-crud-e8c9b",
+        storageBucket: "fir-crud-e8c9b.appspot.com",
+        messagingSenderId: "149782773191"
+      }
